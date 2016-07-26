e9301e4ef7ac0c5d27c46a936fa14a593252a7c3
craig1123 <craigwalker1123@gmail.com>
2016-07-26 15:37:25 -0600

    random food appears

--- app/snake.js
+++ app/snake.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
+
+var food,
+    snake = [];
+
+
   function initiateGameWindow() {
     for (var i = 0; i < 20; i++) {
       for (var j = 0; j < 20; j++) {
-        $('.game-window').append("<div class=cell-square id=cell_" + i + "-" + j +"</div>");
+        $('.game-window').append("<div class=cell-square id=cell_" + i + "-" + j +"></div>");
       }
     }
-    $("#cell_0-0").addClass('snake');
+    $("#cell_0-0, #cell_0-1, #cell_0-2").addClass("snake");
+    randomFood()
   }
+
+  function randomFood() {
+    var row = Math.round(Math.random()*19);
+    var col = Math.round(Math.random()*19);
+    $("#cell_"+row+"-"+col).addClass("food");
+    food = "_"+row+"-"+col;
+  }
+
+
   initiateGameWindow();
 
 });
