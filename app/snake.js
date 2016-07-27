9276410fc1e3b9b2611823b02152d1f00c0c440c
craig1123 <craigwalker1123@gmail.com>
2016-07-27 17:01:35 -0600

    local storage stores high score

--- app/snake.js
+++ app/snake.js
@@ -3,12 +3,32 @@ $(document).ready(function() {
 var food = "",
     snake = ['_0_2', '_0_1', '_0_0'],
     dir =  4;
+    score = 0,
+    highScore = 0,
+    game = 'new';
+
+
+    localStorage.setItem('highScore', highScore);
+    localStorage.getItem('highScore');
+function setHighScore() {
+  if (localStorage.getItem('highScore') < score) {
+    highScore = score
+    localStorage.setItem('highScore', highScore);
+  }
+}
+
+function myScore() {
+  $(".score").text(score);
+  $(".high-score").text(highScore);
+}
 
 
   function initiateGameWindow() {
+    var gameWindow = $('.game-window');
+    gameWindow.html("");
     for (var i = 0; i < 20; i++) {
       for (var j = 0; j < 20; j++) {
-        $('.game-window').append("<div class=cell-square id=cell_" + i + "_" + j +"></div>");
+        gameWindow.append("<div class=cell-square id=cell_" + i + "_" + j +"></div>");
       }
     }
     $("#cell_0_0, #cell_0_1, #cell_0_2").addClass("snake");
@@ -53,26 +73,39 @@ var food = "",
      if(newHead === food) {
        snake.push(tail);
        $('#cell'+food).removeClass("food");
+       score += 1;
+       setHighScore();
        randomFood();
      }
       snake.unshift(newHead);
 
       // end game if snake hits the border OR hits itself
     if (sCol<0 || sRow<0 || sCol>19 || sRow>19 ||  $('#cell'+newHead).hasClass('snake') ){
-      console.log('You lost!');
       snake.push(tail);
       $('#cell'+tail).addClass("snake");
       $('#cell'+snake[1]).addClass("dead-snake");
-
+      game = 'gameOver';
+      function gameOver() {
+        alert("You lose!");
+      }
+      setHighScore();
+      gameOver();
       return;
     }
     else {
       $('#cell'+newHead).addClass('snake');
     }
-      setTimeout(update, 200);
-  }
-  update();
 
+    myScore();
+
+    game = 'inProgress';
+      setTimeout(function() {
+        update();
+      }, 150);
+  }
+
+
+  //logic for directions
   $(document).on('keyup', function(e) {
     if(e.keyCode === 40 && dir !== 3) {
       dir = 1;
@@ -86,5 +119,37 @@ var food = "",
     if(e.keyCode === 39 && dir !== 2) {
       dir = 4;
     }
+    //start game with 'Enter'
+    if (e.keyCode === 13 && game === 'new') {
+      update();
+
+    }
+    //restart game with 'enter'
+    if(e.keyCode === (13 || 40 || 37 || 38 || 39) && game === 'gameOver') {
+      snake = ["_0_2", "_0_1", "_0_0"];
+      food = "";
+      dir = 4;
+      score = 0;
+      game = 'new';
+      initiateGameWindow();
+      update();
+    }
   });
+
+    //Start or restart game with click
+    $(document).click(function (e) {
+      if(game === 'gameOver') {
+        snake = ["_0_2", "_0_1", "_0_0"];
+        food = "";
+        dir = 4;
+        score = 0;
+        game = 'new';
+        initiateGameWindow();
+        update();
+        myScore();
+      }
+      if (game === 'new') {
+        update();
+      }
+    });
 });
