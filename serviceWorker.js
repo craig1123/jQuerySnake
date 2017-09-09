d6c77911d150a606c1db3c658c69475d8108c36d
Craig Walker <cwalker@overstock.com>
2017-09-09 16:55:17 -0600

    service worker fixes

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,41 +1,52 @@
 'use strict';
 
-var cacheVersion = 1;
-var currentCache = {
-  offline: 'offline-cache' + cacheVersion
-};
-var offlineUrl = 'index.html';
+var urlsToCache = [
+    '/',
+    './img/grass.jpg',
+    './img/snake.png',
+    './img/favicon.ico',
+    'https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js',
+    './app/snake.js',
+    './styles/styles.css',
+    './styles/base/normalize.css',
+    './styles/base/reset.css',
+    'index.html',
+];
 
-this.addEventListener('install', function(event) {
+
+var version = 'v1';
+
+self.addEventListener('install', function(event) {
+  console.log('[ServiceWorker] Installed version ', version);
   event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll([
-          './img/grass.jpg',
-          './img/snake.png',
-          './img/favicon.ico',
-          offlineUrl
-      ]);
+    caches.open(version)
+      .then(function(cache) {
+      console.log("opened cache");
+      return cache.addAll(urlsToCache_);
     })
   );
 });
 
-this.addEventListener('fetch', function(event) {
-    // request.mode = navigate isn't supported in all browsers
-    // so include a check for Accept: text/html header.
-    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(function(error) {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
-        );
-    }
-    else {
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request)
-                .then(function (response) {
-                return response || fetch(event.request);
-            })
-        );
-    }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [version];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (version && cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleted old cache');
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
